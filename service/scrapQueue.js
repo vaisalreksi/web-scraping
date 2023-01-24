91ca750afd7d400dc706c8be8375c61cbac86fbb
Vaisal Reksi <reksi@tamasia.co.id>
2023-01-24 10:44:33 +0700

    add url sentiment

--- service/scrapQueue.js
+++ service/scrapQueue.js
@@ -1,17 +1,38 @@
+const axios = require("axios")
+
 const news = require("../config/repository/news")
 
 const scrapQueue = (job, done) => {
     try {
-        news.createNews(job.data, (err) => {
+        getSentiment(job.data.title).then(res => {
+          job.data.sentiment = res.data[0].label
+          news.createNews(job.data, (err) => {
             if (err) {
               console.log(err)
             }
           });
+        })
+
         done()
     } catch (error) {
         console.log(error)
-        throw error
     }
+}
+
+const getSentiment = async function (title, res) {
+  try {
+    let resp = await axios.post(process.env.URL_SENTIMENT, 
+    {data: [title]}, 
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    return resp.data
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = scrapQueue
